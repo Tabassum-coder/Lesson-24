0ed31a7a929555129c37a4f942bcadfbbc2c551a
Tabassum-code <tabuj2706@gmail.com>
2024-09-20 19:37:26 +0530

    JS real time validation

--- script.js
+++ script.js
@@ -1,6 +1,20 @@
-function myfunction(){
-    var a=5;
-    var b=4;
-    var sum=a+b;
-    document.getElementById("p1").innerHTML="Sum of "+a+" and "+b+" is "+sum;
+function validate(){
+    const user = document.getElementById("txtuser").value;
+    const pwd = document.getElementById("txtpwd").value;
+    var msgbox=document.getElementById("msg");
+    let message='';
+    if(user==''){
+        message="Please enter username";
+        msgbox.style.color='red';
+    }
+    else if(pwd===''){
+        message="Please enter the password";
+        msgbox.style.color = 'red';
+    }
+    else{
+        message="Login Successful!";
+        msgbox.style.color = 'green';
+    }
+    msgbox.innerText=message;
+    
 }
